7671504867e002f7420db854674b0bb848d52902
jucrs <cressonjulie@gmail.com>
2016-11-24 13:04:05 +0100

    Nom fonction

--- grid.h
+++ grid.h
@@ -15,8 +15,43 @@ const int SIZE_H = 9;
 const int SIZE_W = 5;
 using TGrid = array<array<int,SIZE_W>,SIZE_H>;
 
+/****************** Nom de la fonction **********************
+* initGrid                                                  *
+******************** Auteur , Dates *************************
+* Groupe / 23 novembre                                      *
+********************* Description ***************************
+* Initialisation d'une grille                               *
+*********************** Entrées *****************************
+* Une grille                                                *
+*********************** Sorties *****************************
+* Une grille contenant que des zéro                         *
+************************************************************/
 void initGrid(TGrid &grid);
+
+/****************** Nom de la fonction **********************
+* convertTo_IJ                                              *
+******************** Auteur , Dates *************************
+* Groupe / 23 novembre                                      *
+********************* Description ***************************
+* Télécharge une image en enlevant une couleur              *
+*********************** Entrées *****************************
+* nom de l'image et les couleurs rouge/vert/bleu            *
+*********************** Sorties *****************************
+* l'image avec la couleur enlevé                            *
+************************************************************/
 void convertTo_IJ (int x, int y,int &i, int &j);
+
+/****************** Nom de la fonction **********************
+* loadImageWithColorKey                                     *
+******************** Auteur , Dates *************************
+* Groupe / 23 novembre                                      *
+********************* Description ***************************
+* Télécharge une image en enlevant une couleur              *
+*********************** Entrées *****************************
+* nom de l'image et les couleurs rouge/vert/bleu            *
+*********************** Sorties *****************************
+* l'image avec la couleur enlevé                            *
+************************************************************/
 void convertTo_Px (int &x, int &y, int i, int j);
 
 void afficherGrid(TGrid grid);
