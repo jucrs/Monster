52694ce53355473eb1409924bcca8268f31eb853
jucrs <cressonjulie@gmail.com>
2016-12-02 06:53:56 +0100

    nom fonction

--- monster.h
+++ monster.h
@@ -16,9 +16,45 @@ struct Monster
     SDL_Rect lecture;
 };
 
+/****************** Nom de la fonction **********************
+* initMonter/initMonsterSleep                               *
+******************** Auteur , Dates *************************
+* Groupe                                                    *
+********************* Description ***************************
+* Initialisation des monstres                               *
+*********************** Entrées *****************************
+* un monstre                                                *
+*********************** Sorties *****************************
+* un monstre                                                *
+************************************************************/
 void initMonster(Monster &m);
 void initMonsterSleep(Monster &m2);
+
+/****************** Nom de la fonction **********************
+* showMoster/showMonsterSleep                               *
+******************** Auteur , Dates *************************
+* Groupe                                                    *
+********************* Description ***************************
+* affiche les monstres                                      *
+*********************** Entrées *****************************
+* monstre, case i,j                                         *
+*********************** Sorties *****************************
+* monstre, écran                                            *
+************************************************************/
 void showMonster(Monster &m, SDL_Surface *screen, int i, int j);
 void showMonsterSleep(Monster &m, SDL_Surface *screen, int i, int j);
+
+/****************** Nom de la fonction **********************
+* mooveMonster                                              *
+******************** Auteur , Dates *************************
+* Groupe                                                    *
+********************* Description ***************************
+* Permet le mouvement du monstre                            *
+*********************** Entrées *****************************
+* la grille                                                 *
+*********************** Sorties *****************************
+* la grille                                                 *
+************************************************************/
+void mooveMonster (TGrid grid);
 #endif // MONSTER
 
